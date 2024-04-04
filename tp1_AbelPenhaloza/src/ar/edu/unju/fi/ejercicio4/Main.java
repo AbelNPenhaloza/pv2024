cc8587d1235a3a2acb7059778aa12500d7576761
Abel Nestor Peñaloza <abelnrpenaloza@gmail.com>
2024-04-04 14:31:50 -0300

    Commit del ejercicio 4 tp1 pv.
    Descripción: Se realizo el ejercicio de calculo del factorial de un
    número ingresado por consola.
    Autor: Abel Peñaloza

---
+++ tp1_AbelPenhaloza/src/ar/edu/unju/fi/ejercicio4/Main.java
@@ -0,0 +1,31 @@
+package ar.edu.unju.fi.ejercicio4;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.print("Ingrese un número del 0 al 10 para calcular su factorial: ");
+	int numero = sc.nextInt();
+	
+	if(numero < 0 || numero > 10) {
+		System.out.println("El número debe estar dentro del rango [0, 10]");
+		return; // Salir del programa si el numero no esta en el rango valido.
+	}
+	
+	//Calculo del factorial utilizando un bucle while
+	int factorial = 1;
+	int i= 1;
+	while(i<= numero) {
+		factorial*= i;
+		i++;
+	}
+	
+	//Mostrar el resultado por consola
+	System.out.println("El factorial de "+ numero + " es: "+ factorial);
+
+	}
+
+}
