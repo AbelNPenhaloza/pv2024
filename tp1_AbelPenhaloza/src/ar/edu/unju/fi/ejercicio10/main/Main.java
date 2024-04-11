7ae0112c80ed8057fd7bf71c79da337f9375c589
Abel Nestor Peñaloza <abelnrpenaloza@gmail.com>
2024-04-11 13:50:22 -0300

    Se finaliza con punto 10
    Descripcion: Se implemento switch y try catch.
    Autor: Abel N Penhaloza

--- tp1_AbelPenhaloza/src/ar/edu/unju/fi/ejercicio10/main/Main.java
+++ tp1_AbelPenhaloza/src/ar/edu/unju/fi/ejercicio10/main/Main.java
@@ -1,15 +1,56 @@
 package ar.edu.unju.fi.ejercicio10.main;
 
+import java.util.Scanner;
+
 import ar.edu.unju.fi.ejercicio10.model.Pizza;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Pizza pizza1= new Pizza();
 		
+		Scanner sc = new Scanner(System.in);
 		
+		//Crear tres objetos de la clase Pizza
+		for(int i=1; i<=3; i++) {
+						
+			Pizza pizza = new Pizza();
+			
+			// Ingreso de datos de la pizza
+			int diametro=0;
+			boolean diametroValido= false;
+			
+			while(!diametroValido) {
+				try {
+					System.out.print("Ingreso el diametro de la Pizza [20,30,40]: ");
+					diametro = sc.nextInt();
+					if(diametro == 20 || diametro== 30 || diametro == 40) {
+						diametroValido= true;
+					} else {
+						System.out.println("Diametro no valido. Ingrese un DIAMETRO VALIDO");
+					}
+				}catch (NumberFormatException e) {
+					System.out.println("Ingrese un valor entero");
+				}
+			}
+			pizza.setDiametro(diametro);
+			System.out.print("¿Lleva ingredientes Especiales? (true / false): ");
+			pizza.setIngredienteEspeciales(sc.nextBoolean());
+			
+			//Calcular precio y area.
+			pizza.calcularPrecio();
+			pizza.calcularArea();
+			//Mostrar los resultados
+			System.out.println("** Pizza " + i + " **");
+			System.out.println("Diametro= "+(int)pizza.getDiametro());
+			System.out.println("Ingredientes Especiales= "+ pizza.isIngredienteEspeciales());
+			System.out.println("Precio de la Pizza= $ "+ pizza.getPrecio());
+			System.out.println("Area de la Pizza= "+ pizza.getArea());
+			
+			sc.nextLine();
+		}
 		
+		sc.close();
+				
 	}
 
 }
--- tp1_AbelPenhaloza/src/ar/edu/unju/fi/ejercicio10/model/Pizza.java
+++ tp1_AbelPenhaloza/src/ar/edu/unju/fi/ejercicio10/model/Pizza.java
@@ -1,20 +1,86 @@
 package ar.edu.unju.fi.ejercicio10.model;
 
 public class Pizza {
-	private double diametro;
+	private int diametro;
 	private double precio;
 	private double area;
-	private boolean ingredienteEspeciales;
+	private boolean ingredientesEspeciales;
+	
+	//Constantes para los adicionales de ingredientes especiales.
+	
 	public static final double ESPECIAL20 = 500;
 	public static final double ESPECIAL30 = 750;
 	public static final double ESPECIAL40 = 1000;
 	
-	public double calcularPrecio() {
-		//if(diametro ==20 && !ingredientesEspeciales) {
-			precio=4500;
-		//}else if(diametro ==20 && ingredientesEspeciales) {
-			precio= 4500 + ESPECIAL20;
-		//}
-		return 0;
+	/**
+	 * Constructor por defecto
+	 */
+	public Pizza() {}
+	
+	// Metodo para calcular el precio de la pizza
+	public void calcularPrecio() {
+		switch (diametro) {
+			case 20:
+				precio= 4500;
+				if (ingredientesEspeciales) {
+					precio+= ESPECIAL20;
+				}
+				break;
+			case 30:
+				precio= 4800;
+				if(ingredientesEspeciales) {
+					precio += ESPECIAL30;
+				}
+				break;
+			case 40:
+				precio = 5500;
+				if(ingredientesEspeciales) {
+					precio += ESPECIAL30;
+				}
+				break;
+			default:
+				System.out.println("Diametro de la pizza no valido.");
+				break;
+		}
+		
 	}
+	
+	//Metodo que actualiza el valor del atributo area.
+	public void calcularArea() {
+		area = Math.PI * Math.pow((diametro/2.0), 2);		
+	}
+	//Metodos accesores
+		
+	public double getDiametro() {
+		return diametro;
+	}
+
+	public void setDiametro(int diametro) {
+		this.diametro = diametro;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public double getArea() {
+		return area;
+	}
+
+	public void setArea(double area) {
+		this.area = area;
+	}
+
+	public boolean isIngredienteEspeciales() {
+		return ingredientesEspeciales;
+	}
+
+	public void setIngredienteEspeciales(boolean ingredientesEspeciales) {
+		this.ingredientesEspeciales = ingredientesEspeciales;
+	}
+	
 }
